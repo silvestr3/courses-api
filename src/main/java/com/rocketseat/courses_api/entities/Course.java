03a066c791616ce1787cf49cff44f2d0a160b313
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-29 15:58:16 -0300

    feat: Define courses entity

---
+++ src/main/java/com/rocketseat/courses_api/entities/Course.java
@@ -0,0 +1,30 @@
+package com.rocketseat.courses_api.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private String category;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
